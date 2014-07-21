7e4b457b6cae6c1eb16693eb95b485c3261e7ab6
son <son@lincolnix.net>
2014-07-21 09:05:21 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MontessorImage::Application.config.secret_key_base = '450ad7c1992311adf56bd27289ce47e272fea16474eb19dcaa4408f3dd8b49f7b9e2fe66832751fa01c703ee3e5f0066f8da6440aff9fd2e839004b560edcd12'
